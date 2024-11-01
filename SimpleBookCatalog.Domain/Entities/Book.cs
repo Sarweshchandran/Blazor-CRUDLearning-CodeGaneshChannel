0ad5890298e0ba34f0fb40839d97b17c55dd9480
sarwe <sarwe@SARWESHCHANDRAN>
2024-11-01 15:23:29 +0530

    Add project files.

---
+++ SimpleBookCatalog.Domain/Entities/Book.cs
@@ -0,0 +1,23 @@
+﻿using SimpleBookCatalog.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBookCatalog.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage="Please provide a title")]
+        [StringLength(100)]
+        public string? Title { get; set; }
+        
+        [Required(ErrorMessage ="Please provide the author's name")]
+        [StringLength(100)]
+        public string? Author { get; set; }
+        
+        public DateTime? PublicationDate { get; set; }
+        [EnumDataType(typeof(Category),ErrorMessage ="Please Select a Category")]
+        public Category Category { get; set; }
+        
+    }
+}
---
+++ SimpleBookCatalog.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using SimpleBookCatalog.Application.Interfaces;
+using SimpleBookCatalog.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using SimpleBookCatalog.Domain.Entities;
+
+namespace SimpleBookCatalog.Infrastructure.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly SimpleBookCatalogDbContext context;
+        public BookRepository(IDbContextFactory<SimpleBookCatalogDbContext> factory)
+        {
+            context = factory.CreateDbContext();
+        }
+
+        public async Task AddAsync(Book book)
+        {
+            context.Books.Add(book);
+            await context.SaveChangesAsync();
+        }
+    }
+}
